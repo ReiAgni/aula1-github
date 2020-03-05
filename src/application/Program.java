2c06cc82c8be25b836d1da6ab0f749b5445c37c1
WilliamBSilva <williambsilvati@gmail.com>
2020-03-04 23:03:20 -0300

    Refatorando

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,29 +5,25 @@ import java.util.Scanner;
 
 import util.CurrencyCoverter;
 
-
 public class Program {
-	
-	
 
 	public static void main(String[] args) {
-		
+
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		CurrencyCoverter conv = new CurrencyCoverter();
-		
+
 		System.out.print("What is the dollar price? ");
 		double priceDollar = sc.nextDouble();
-		
+
 		System.out.print("How many dollars will be bought? ");
 		double beBought = sc.nextDouble();
-		
+
 		double resultado = conv.convertendo(priceDollar, beBought);
-		
-		System.out.printf("Amount to be paid in reais = %.2f%n ", resultado );
-		
+
+		System.out.printf("Amount to be paid in reais = %.2f%n ", resultado);
+
 		sc.close();
 	}
-	
-	
+
 }
