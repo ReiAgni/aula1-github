12c947ab8c1c143bd6d78d22613a110e66f0f1c8
ReiAgni <williambsilvati@gmail.com>
2020-03-04 22:43:47 -0300

    Projeto criado

---
+++ src/util/CurrencyCoverter.java
@@ -0,0 +1,13 @@
+package util;
+
+public class CurrencyCoverter {
+	
+	public double convertendo(double dolar, double bought) {
+		double calc1 = dolar * bought;
+		double calc2 = calc1 * 0.06;
+		double result = calc2 + calc1;
+		
+		return result;
+	}
+	
+}
